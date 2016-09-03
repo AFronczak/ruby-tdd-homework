8c29d0b70bdc4f8b942aac3f35cd87e99109a0c0
Adam Fronczak <Fronczak.Adam@gmail.com>
2016-09-03 15:48:24 -0400

    test for dimes completed

--- change.rb
+++ change.rb
@@ -1,7 +1,8 @@
 class Coin
   def make_change(num)
     coin_array = [0,0,0,0]
-    coin_array[2] = num / 5
+    coin_array[1] = num / 10
+    coin_array[2] = (num % 10) / 5
     coin_array[3] = num % 5
     return coin_array
   end
--- unit_homework.rb
+++ unit_homework.rb
@@ -13,15 +13,20 @@ class ChangeTest < MiniTest::Test
   end
   def test_if_shows_nickles
     # change shows nickles
+    assert_equal [0,0,1,0], Coin.new.make_change(5)
     assert_equal [0,0,1,1], Coin.new.make_change(6)
+    assert_equal [0,0,1,4], Coin.new.make_change(9)
     skip
   end
   def test_if_shows_dimes
     # change shows dimes
-    skip
+    assert_equal [0,1,0,0], Coin.new.make_change(10)
+    assert_equal [0,1,1,1], Coin.new.make_change(16)
+    assert_equal [0,2,1,1], Coin.new.make_change(26)
   end
   def test_if_shows_quarters
     # change shows quarters
+    assert_equal [1,0,0,0], Coin.new.make_change(25)
     skip
   end
 end
