c75afa6231c74b42683b59c0231f6aae1febe575
Adam Fronczak <Fronczak.Adam@gmail.com>
2016-09-04 17:57:04 -0400

    testing of numbers 1-10 complete

--- roman.rb
+++ roman.rb
@@ -1,8 +1,18 @@
 class Roman
-  text = [ 1 => "I"]
-  def numerals(num)
-    if num = 1
-      "I"
+  # for roman numerals under 10... provide correct numeral
+   def numerals(num)
+    if num < 4
+      num = "I" * num
+    elsif num == 4
+      num = "IV"
+    elsif num == 5
+      num = "V"
+    elsif num > 5 && num < 9
+      num = "V" + ("I" * (num % 5))
+    elsif num == 9
+      num = "IX"
+    elsif num == 10
+      num = "X"
     end
   end
 end
--- unit_homework_pt_2.rb
+++ unit_homework_pt_2.rb
@@ -4,7 +4,19 @@ require_relative 'roman'
 class RomanTest < MiniTest::Test
   # Given a arabic number (our familiar number system)
   # under 3,000 convert it to the corresponding roman numeral.
-  def test_smallest_numerals
+  def test_smallest_numerals_to_ten
     assert_equal "I", Roman.new.numerals(1)
+    assert_equal "II", Roman.new.numerals(2)
+    assert_equal "III", Roman.new.numerals(3)
+    assert_equal "IV", Roman.new.numerals(4)
+    assert_equal "V", Roman.new.numerals(5)
+    assert_equal "VI", Roman.new.numerals(6)
+    assert_equal "VII", Roman.new.numerals(7)
+    assert_equal "VIII", Roman.new.numerals(8)
+    assert_equal "IX", Roman.new.numerals(9)
+    assert_equal "X", Roman.new.numerals(10)
+  end
+
+  def test_double_digit_numbers
   end
 end
